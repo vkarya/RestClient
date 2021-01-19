9aa9d84dff40006730cb0b316e9cefe32cf63c64
Vaibhav Arya <vaibhavkumar.arya@gmail.com>
2021-01-19 11:23:56 +0530

    changes

---
+++ TravelClient/src/com/afkl/travel/exercise/client/TravelClient.java
@@ -0,0 +1,47 @@
+package com.afkl.travel.exercise.client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+public class TravelClient {
+
+    public static void main(String[] args) {
+        try {
+
+          URL restUrl = new URL("http://localhost:8080/travel/locations/country/US");
+          
+          HttpURLConnection conn = (HttpURLConnection) restUrl.openConnection();
+          conn.setRequestMethod("GET");
+          conn.setRequestProperty("Accept", "application/json");
+          conn.setRequestProperty("Accept-Language", "en-US");
+          conn.setRequestProperty("userName", "someuser");
+          conn.setRequestProperty("password", "psw");
+          conn.setRequestProperty("authorization", "Basic c29tZXVzZXI6cHN3");
+          if (conn.getResponseCode() != 200) {
+              throw new RuntimeException("Failed : HTTP error code : "
+                      + conn.getResponseCode());
+          }
+          BufferedReader br = new BufferedReader(new InputStreamReader(
+              (conn.getInputStream())));
+
+          String output;
+          System.out.println("Response from Service:");
+          while ((output = br.readLine()) != null) {
+              System.out.println(output);
+          }
+          conn.disconnect();
+        }catch (Exception e) {
+        	e.printStackTrace();
+        }
+      }
+
+}
